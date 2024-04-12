fe396050f54f385fd73ba094874d76502d1f677e
Nosk0v <miluykovalexandr@gmail.com>
2024-04-12 16:02:05 +0500

    Добавлено подключение к базе "Trade"

--- View/Authorization.xaml.cs
+++ View/Authorization.xaml.cs
@@ -14,14 +14,32 @@ using System.Windows.Shapes;
 
 namespace JewerlyStore.View
 {
-    /// <summary>
-    /// Логика взаимодействия для Authorization.xaml
-    /// </summary>
+    
     public partial class Authorization : Window
     {
+        private readonly Database.TradeEntities entities;
+        private Database.User user;
+
         public Authorization()
         {
             InitializeComponent();
+            entities = new Database.TradeEntities();
+        }
+
+        private void OnSignClick(object sender, RoutedEventArgs e)
+        {
+            string login = tbLogin.Text.Trim();
+            string password = tbPassword.Password.Trim();
+            if (login.Length < 1 || password.Length < 1 )
+            {
+                MessageBox.Show("Введите логин и пароль");
+                return;
+            }
+            user = entities.Users.Where(u => u.UserLogin ==  login && u.UserPassword == password).FirstOrDefault();
+            if (user == null) 
+            {
+                MessageBox.Show("Неверный логин или пароль");
+            }
         }
     }
 }
